79f79fef8e2e026c4f3348d70b5c3f4020484067
Tim Ngeno <timngeno424@gmail.com>
2024-09-20 18:23:26 +0300

    handle data conversion and translation for models

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Customer, Order
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ["id", "name", "code", "phone_number"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ["id", "customer", "item", "amount", "time"]
