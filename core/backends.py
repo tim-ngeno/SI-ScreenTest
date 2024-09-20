d928e3a0e0079869cc4b8f7a93e71316f3c3e6cb
Tim Ngeno <timngeno424@gmail.com>
2024-09-20 18:22:35 +0300

    implement authentication backend service

---
+++ core/backends.py
@@ -0,0 +1,62 @@
+import logging
+from django.core.exceptions import SuspiciousOperation
+from django.urls import reverse
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from mozilla_django_oidc.utils import absolutify
+
+LOGGER = logging.getLogger(__name__)
+
+
+class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
+    def authenticate(self, request, **kwargs):
+        self.request = request
+        if not self.request:
+            return None
+
+        state = self.request.GET.get("state")
+        code = self.request.GET.get("code")
+        nonce = kwargs.pop("nonce", None)
+
+        if not code or not state:
+            return None
+
+        reverse_url = self.get_settings(
+            "OIDC_AUTHENTICATION_CALLBACK_URL", "oidc_authentication_callback"
+        )
+
+        token_payload = {
+            "client_id": self.OIDC_RP_CLIENT_ID,
+            "client_secret": self.OIDC_RP_CLIENT_SECRET,
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": absolutify(self.request, reverse(reverse_url)),
+        }
+
+        # Get the token
+        token_info = self.get_token(token_payload)
+        id_token = token_info.get("id_token")
+        access_token = token_info.get("access_token")
+        refresh_token = token_info.get("refresh_token")
+
+        # Validate the token
+        payload = self.verify_token(id_token, nonce=nonce)
+
+        if payload:
+            self.store_tokens(access_token, id_token, refresh_token)
+            try:
+                return self.get_or_create_user(access_token, id_token, payload)
+            except SuspiciousOperation as e:
+                LOGGER.warning(f"Failed to get or create user: {e}")
+                return None
+
+    def store_tokens(self, access_token, id_token, refresh_token):
+        session = self.request.session
+
+        if self.get_settings("OIDC_STORE_ACCESS_TOKEN", True):
+            session["oidc_access_token"] = access_token
+
+        if self.get_settings("OIDC_STORE_ID_TOKEN", False):
+            session["oidc_id_token"] = id_token
+
+        if self.get_settings("OIDC_STORE_REFRESH_TOKEN", True):
+            session["oidc_refresh_token"] = refresh_token
