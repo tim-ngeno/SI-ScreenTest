747d96c45200e27d3828b44686fb15c5b82ae5ef
Tim Ngeno <timngeno424@gmail.com>
2024-09-23 19:50:07 +0300

    update urls and remove commented paths

--- api/urls.py
+++ api/urls.py
@@ -4,12 +4,10 @@ from .oidc_settings import oidc_callback
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r"customers", views.CustomerViewSet)
-router.register(r"orders", views.OrderViewSet)
+router.register(r"customers", views.CustomerViewSet, basename="customer")
+router.register(r"orders", views.OrderViewSet, basename="order")
 
 urlpatterns = [
-    # path("", views.get_customers, name="customers"),
     path("", include(router.urls)),
-    # path("orders/", views.get_orders, name="orders"),
     path("oidc/callback/", oidc_callback, name="oidc_callback"),
 ]
