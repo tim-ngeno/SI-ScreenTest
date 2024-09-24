27a9ccb594db669e4d6bac84b748f401b5d2c57c
Tim Ngeno <timngeno424@gmail.com>
2024-09-24 12:26:42 +0300

    update tests

--- api/tests.py
+++ api/tests.py
@@ -19,21 +19,21 @@ class CustomerTestCase(APITestCase):
         self.assertEqual(response.data[0]["name"], self.customer.name)
 
 
-class OrderTestCase(APITestCase):
-    def setUp(self):
-        self.customer = Customer.objects.create(
-            name="Test Customer",
-            code="121-ASDAF242",
-        )
-        self.url = reverse("order-list")
+# class OrderTestCase(APITestCase):
+#     def setUp(self):
+#         self.customer = Customer.objects.create(
+#             name="Test Customer",
+#             code="121-ASDAF242",
+#         )
+#         self.url = reverse("order-list")
 
-    # def test_create_order(self):
-    #     data = {
-    #         "customer": self.customer.id,
-    #         "item": "Test Item",
-    #         "amount": 100.93,
-    #     }
-    #     response = self.client.post(self.url, data, format="json")
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(Order.objects.count(), 1)
-    #     self.assertEqual(Order.objects.first().item, "Test Item")
+# def test_create_order(self):
+#     data = {
+#         "customer": self.customer.id,
+#         "item": "Test Item",
+#         "amount": 100.93,
+#     }
+#     response = self.client.post(self.url, data, format="json")
+#     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#     self.assertEqual(Order.objects.count(), 1)
+#     self.assertEqual(Order.objects.first().item, "Test Item")
