66f3fb3fe573a42ed438bc0c5c1bdf7d4307b223
Tim Ngeno <timngeno424@gmail.com>
2024-09-26 11:38:21 +0300

    update login middleware

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "core.login_middleware.LoginRequiredMiddleware",
+    # "core.login_middleware.LoginRequiredMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
