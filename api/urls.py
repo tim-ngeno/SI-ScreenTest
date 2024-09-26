28298c1a8fb4ffe074acf4312881ff5f42f30f8c
Tim Ngeno <timngeno424@gmail.com>
2024-09-26 09:15:50 +0300

    update url endpoints

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r"customers", views.CustomerViewSet, basename="customer")
 router.register(r"orders", views.OrderViewSet, basename="order")
 
+
 urlpatterns = [
     path("", include(router.urls)),
     path("oidc/callback/", oidc_callback, name="oidc_callback"),
