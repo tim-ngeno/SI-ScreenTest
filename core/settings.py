f96fd8d4016c2fa023cbc1a75b73a379e2317691
Tim Ngeno <timngeno424@gmail.com>
2024-10-03 11:55:29 +0300

    update django settings variables

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = getenv("DJANGO_SECRET_KEY", get_random_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DJANGO_DEBUG", "False").lower() in ("true", "1", "t")
-# DEBUG = True
+
 ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
